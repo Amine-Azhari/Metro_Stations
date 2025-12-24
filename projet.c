71b25eb1313d41030ec6a04a2768f4b2461caff2
ABAHMANE Hadil <hadil.abahmane@uha.fr>
2025-12-24 19:13:35 +0100

    3functions of sorting implemented XD

--- projet.c
+++ projet.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <limits.h>
 #include <string.h>
+#include <time.h> //hadi bash ndiru clock
 
 #define NUM_STATIONS 125
 #define HASH_SIZE 127
@@ -18,6 +19,11 @@ typedef struct HashEntry {
     int id;
     struct HashEntry *next;
 } HashEntry;
+
+typedef struct {
+    int id_station;  
+    int degre;       
+} StationDegre;
 
 Edge** adjacency_list;
 char** stations;
@@ -270,6 +276,191 @@ void find_shortest_path(char *start_input, char *end_input) {
     printf("\n═══════════════════════════════════════\n\n");
 }
 
+//hadi function dyal tri pra selectoin
+void tri_selection(StationDegre arr[], int n, int* comparisons, int* swaps) {
+    *comparisons = 0;  
+    *swaps = 0;
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+
+        for (int j = i + 1; j < n; j++) {
+            (*comparisons)++;
+            if (arr[j].degre < arr[min_idx].degre) {
+                min_idx = j;  
+            }
+        }
+
+        if (min_idx != i) {
+            StationDegre temp = arr[i]; 
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp; 
+            (*swaps)++;
+            
+        }
+    }        
+
+}
+
+//function dyal tri par insertion 
+void tri_insertion(StationDegre arr[], int n, int* comparisons, int* swaps) {
+    *comparisons = 0;  
+    *swaps = 0;
+    for (int i = 1; i < n; i++) {
+        StationDegre key = arr[i]; 
+        int j = i - 1;
+        (*comparisons)++;
+
+        while (j >= 0 && arr[j].degre > key.degre) {
+            if (j != i - 1) (*comparisons)++;
+            arr[j + 1] = arr[j]; 
+            (*swaps)++;
+            j--; 
+
+        }
+
+        if (j + 1 != i) { 
+            arr[j + 1] = key;
+            (*swaps)++;
+
+        }
+
+    }
+}
+
+//function dyal quicksort
+int partition(StationDegre arr[], int low, int high, int* comparisons, int* swaps) {
+    int pivot = arr[high].degre;  
+    int i = low - 1;   
+
+    for (int j = low; j <= high - 1; j++) {
+        (*comparisons)++;
+
+        if (arr[j].degre <= pivot) {
+            i++;
+            if (i != j) {
+                StationDegre temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                (*swaps)++;  
+            }
+
+        }
+
+
+    }
+
+    if (i + 1 != high) {
+        StationDegre temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        (*swaps)++;
+    }
+    return i+1;
+
+}
+
+//function récurssivve dyal tri quicksort
+void tri_rapide_recursif(StationDegre arr[], int low, int high, int* comparisons, int* swaps) {
+    if (low < high) {
+        int pi = partition(arr, low, high, comparisons, swaps);
+        tri_rapide_recursif(arr, low, pi - 1, comparisons, swaps);  
+        tri_rapide_recursif(arr, pi + 1, high, comparisons, swaps);
+    }
+}
+
+void tri_rapide(StationDegre arr[], int n, int* comparisons, int* swaps) {
+    *comparisons = 0;  
+    *swaps = 0;        
+    
+    
+    tri_rapide_recursif(arr, 0, n - 1, comparisons, swaps);
+}
+
+void afficher_stations_triees(int choix_tri) {
+    int stations_valides = 0;
+    for (int i = 0; i < NUM_STATIONS; i++) {
+        if (stations[i] != NULL) {
+            stations_valides++;
+        }
+    }
+
+    StationDegre *tab_stations = malloc(stations_valides * sizeof(StationDegre));
+    if (tab_stations == NULL) {
+        printf("Erreur d'allocation mémoire\n");
+        return;  
+    }
+    int index = 0;
+    for (int i = 0; i < NUM_STATIONS; i++) {
+        if (stations[i] != NULL) {
+            tab_stations[index].id_station = i;
+            tab_stations[index].degre = edge_count[i];
+            index++;
+        }
+
+    }
+
+    int comparisons = 0, swaps = 0;  
+    char* nom_tri = "";             
+    clock_t start, end;              
+    double cpu_time_used;   
+
+    start = clock();
+    switch(choix_tri) {
+        case 1:
+            nom_tri = "Tri par sélection";
+            tri_selection(tab_stations, stations_valides, &comparisons, &swaps);
+            break;
+        case 2:
+            nom_tri = "Tri par insertion";
+            tri_insertion(tab_stations, stations_valides, &comparisons, &swaps);
+            break;
+        case 3:
+            nom_tri = "Tri rapide (Quicksort)";
+            tri_rapide(tab_stations, stations_valides, &comparisons, &swaps);
+            break;
+        default:
+            
+            printf("Choix de tri invalide\n");
+            free(tab_stations);  
+            return;
+    }
+    
+    end = clock();  
+    
+    // hena kandir calcul dyal clock
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    
+    
+    printf("\n══════════════════════════════════════════════════════════\n");
+    printf("STATIONS TRIÉES PAR DEGRÉ CROISSANT (%s)\n", nom_tri);
+    printf("══════════════════════════════════════════════════════════\n");
+    
+    printf("%-4s | %-40s | %-6s\n", "ID", "NOM", "DEGRÉ");
+    printf("------|------------------------------------------|--------\n");
+    
+    for (int i = 0; i < stations_valides; i++) {
+        printf("%-4d | %-40s | %-6d\n", 
+               tab_stations[i].id_station,                         
+               stations[tab_stations[i].id_station],               
+               tab_stations[i].degre);                             
+    }
+    
+    printf("\n══════════════════════════════════════════════════════════\n");
+    printf("STATISTIQUES DU TRI :\n");
+    printf("• Algorithme utilisé   : %s\n", nom_tri);
+    printf("• Nombre de stations   : %d\n", stations_valides);
+    printf("• Nombre de comparaisons : %d\n", comparisons);
+    printf("• Nombre d'échanges      : %d\n", swaps);
+    printf("• Temps d'exécution      : %.6f secondes\n", cpu_time_used);
+    printf("══════════════════════════════════════════════════════════\n\n");
+    
+    free(tab_stations);
+
+
+
+
+}
+
 
 
 int main(int argc, char** argv)
@@ -304,7 +495,7 @@ int main(int argc, char** argv)
     while (fgets(myString, sizeof(myString), fptr)) {
         if (strncmp(myString, "STATION;", 8) == 0) {
 
-            char *type = strtok(myString, ";"); // "STATION"
+            char *type = strtok(myString, ";"); 
             char *id   = strtok(NULL, ";");
             char *name = strtok(NULL, ";");
 
@@ -334,7 +525,7 @@ int main(int argc, char** argv)
                 int time =atoi(time_str);
                 
                 if (edge_count[start_station] >= 10) {
-                    // Réallouer si nécessaire
+                    
                     adjacency_list[start_station] = realloc(
                         adjacency_list[start_station], 
                         (edge_count[start_station] + 5) * sizeof(Edge)
@@ -394,9 +585,26 @@ int main(int argc, char** argv)
                     break;
                 }
             
-            case (4): 
-                printf("Tri par degré - Mazal madethash but i will dw ^^ \n");
+            case (4): {
+                int choix_tri;
+                printf("\n===== TRI DES STATIONS PAR DEGRÉ =====\n");
+                printf("Choisissez l'algorithme de tri :\n");
+                printf("1 - Tri par sélection\n");
+                printf("2 - Tri par insertion\n");
+                printf("3 - Tri rapide (Quicksort)\n");
+                printf("Votre choix : ");
+                scanf("%d", &choix_tri);
+                getchar(); 
+                
+                
+                if (choix_tri >= 1 && choix_tri <= 3) {
+                    afficher_stations_triees(choix_tri);
+                } else {
+                    printf("Choix invalide ! Veuillez choisir 1, 2 ou 3.\n");
+                }
                 break;
+
+            }
 
             case 0:
                 printf("Au revoir !\n");
